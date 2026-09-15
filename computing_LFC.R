8f3fd7440283152b186221a60e5c08dcd58c6c4d
linaskog <skoglina7@gmail.com>
2026-09-15 10:57:17 +0200

    First attempt at modelling with XGBoost, y = LFC, x = GC content, position data

--- computing_LFC.R
+++ computing_LFC.R
@@ -1,6 +1,6 @@
 
 # Reading in Packages -----------------------------------------------------
-
+library(xgboost)
 library(readxl)
 library(tidyverse)
 library(stringr)
@@ -9,26 +9,26 @@ library(DESeq2)
 
 # Reading in Data ---------------------------------------------------------
 
-excel_sheets("41467_2018_5506_MOESM4_ESM.xlsx")
+excel_sheets("~/Advanced-Bioinformatics/41467_2018_5506_MOESM4_ESM.xlsx")
 
 # Raw Count Data
-gecko <- read_excel("41467_2018_5506_MOESM4_ESM.xlsx", sheet = "C. GECKO_BC3_BJAB" )
+gecko <- read_excel("~/Advanced-Bioinformatics/41467_2018_5506_MOESM4_ESM.xlsx", sheet = "C. GECKO_BC3_BJAB" )
 
 # sgRNA Sequences
-sequences <- read_excel("41467_2018_5506_MOESM4_ESM.xlsx", sheet = "F. GeCKOlibrary2")
-
+sequences <- read_excel("~/Advanced-Bioinformatics/41467_2018_5506_MOESM4_ESM.xlsx", sheet = "F. GeCKOlibrary2")
 
 # Merging Data
-new_gecko <- merge(gecko, sequences, c("UID"))
+gecko <- merge(gecko, sequences, c("UID"))
 
 
 
 # Computing Log2 Fold Change ----------------------------------------------
 
-raw_counts <- as.matrix(new_gecko[, c("BC-3_Day0_Rep1", "BC-3_Day0_Rep2", 
+raw_counts <- as.matrix(gecko[, c("BC-3_Day0_Rep1", "BC-3_Day0_Rep2", 
                                       "BC-3_Day0_Rep3","BC-3_Day14_Rep1", 
                                       "BC-3_Day14_Rep2", "BC-3_Day14_Rep3")])
-rownames(raw_counts) <- new_gecko$UID
+
+rownames(raw_counts) <- gecko$UID
 
 condition_data <- data.frame(
   condition = factor(
@@ -49,6 +49,96 @@ dds <- DESeq(dds)
 
 res <- results(dds, contrast = c("condition", "Day14", "Day0"))
 
-new_gecko$LFC <- res[new_gecko$UID, "log2FoldChange"]
+gecko$LFC <- res[gecko$UID, "log2FoldChange"]
+
+# Filter out infinite LFC values
+valid <- is.finite(gecko$LFC)
+
+gecko_model <- gecko[valid, , drop=FALSE]
+
+# Setting dependent variable y
+y <- gecko_model$LFC
+
+hist(y, breaks = 50)
+
+# Computing Independent Variables -----------------------------------------
+
+# Computing GC content
 
 
+gecko_model$GC <- (str_count(gecko_model$Guide, "G") + 
+                     str_count(gecko_model$Guide, "C")
+                   )/str_length(gecko_model$Guide)
+hist(gecko_model$GC, breaks = 20)
+
+# Transforming sequence to numerical representations for each position
+
+sequence_matrix <- str_split_fixed(
+  gecko_model$Guide, 
+  pattern = "", 
+  n = 20)
+
+X <- do.call(
+  cbind,
+  lapply(1:20, function(i) {
+    model.matrix(~ sequence_matrix[, i] - 1)
+  })
+)
+
+colnames(X) <- paste0(
+  rep(paste0("pos", 1:20), each = 4),
+  "_",
+  rep(c("A", "C", "G", "T"), 20)
+)
+
+X <- cbind(X, GC = gecko_model$GC)
+
+
+# Splitting Data ----------------------------------------------------------
+
+set.seed(2026)
+
+genes <- unique(gecko_model$Gene.y)
+
+train_genes <- sample(genes, floor(0.8 * length(genes)))
+test_genes <- setdiff(genes, train_genes)
+
+train_index <- which(gecko_model$Gene.y %in% train_genes)
+test_index <- which(gecko_model$Gene.y %in% test_genes)
+
+dtrain <- xgb.DMatrix(
+  X[train_index, , drop = FALSE],
+  label = y[train_index]
+)
+
+dtest <- xgb.DMatrix(
+  X[test_index, , drop = FALSE],
+  label = y[test_index]
+)
+
+
+# XGBoost -----------------------------------------------------------------
+
+model <- xgb.train(
+  data = dtrain,
+  nrounds = 1000,
+  params = xgb.params(
+    objective = "reg:squarederror",
+    nthread = 1,
+    max_depth = 4,
+    eta = 0.02,
+    min_child_weight = 10,
+    subsample = 0.7,
+    colsample_bytree = 0.8
+  )
+)
+
+# Predicting with model
+
+pred_train <- predict(model, dtrain)
+pred_test <- predict(model, dtest)
+
+cor(y[train_index], pred_train, method = "spearman")
+cor(y[test_index], pred_test, method = "spearman")
+
+xgb.importance(model)
